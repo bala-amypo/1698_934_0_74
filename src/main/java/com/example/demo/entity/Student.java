3fde6b3846e4c85a873ac4a4fba1670eb0e52568
bala-amypo <bala@amypo.in>
2025-12-17 06:20:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -2,7 +2,7 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
-import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
